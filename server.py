57f77f7e070db1112ec863997de97e7c4ea64568
James Teddy JR <ebhamenteddyjr@gmail.com>
2021-11-28 03:48:20 +0100

    First codes

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+@app.route("/")
+def _world():
+    return "<p>Hello, World!</p>"
+
+
+
+
+
+
